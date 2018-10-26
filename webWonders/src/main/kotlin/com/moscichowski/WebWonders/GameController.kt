6139afb5afe1e55a322c780903e9ab406c8569d9
Marek Mościchowski <moscich@gmail.com>
2018-10-26 12:17:10 +0200

    returning index of newly created game

--- webWonders/src/main/kotlin/com/moscichowski/WebWonders/GameController.kt
+++ webWonders/src/main/kotlin/com/moscichowski/WebWonders/GameController.kt
@@ -4,15 +4,9 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import com.moscichowski.wonders.*
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.jdbc.core.JdbcTemplate
-import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
-import java.sql.PreparedStatement
-import com.oracle.util.Checksums.update
 import org.springframework.web.bind.annotation.RestController
-import org.postgresql.util.PGobject
-
-
 
 
 @RestController
@@ -22,9 +16,9 @@ class GameController {
     lateinit var jdbcTemplate: JdbcTemplate
 
     @RequestMapping(method = [RequestMethod.POST])
-    fun postAction(): String {
+    fun postAction(): Any? {
         val wonderList = listOf(
-            Wonder("First wonder", Resource(1,2,3), features = listOf(Customs)),
+            Wonder("Via Appia", Resource(1,2,3), features = listOf(AddGold(3), RemoveGold, ExtraTurn, VictoryPoints(3))),
             Wonder("Second wonder", Resource(3), features = listOf(DestroyBrownCard)),
             Wonder("Third shieeet", Resource(1,2), features = listOf(DestroySilverCard)),
             Wonder("Everybody dance", Resource(2,glass = 1), features = listOf(ExtraTurn))
@@ -33,13 +27,11 @@ class GameController {
         val objectMapper = ObjectMapper()
         val writeValueAsString = objectMapper.writeValueAsString(wonderList)
 
-        val jsonbObj = PGobject()
-        jsonbObj.type = "json"
-        jsonbObj.value = "{\"Hehe\" : \"Num\"}"
-
-        jdbcTemplate.batchUpdate("insert into games (initial) values ('$writeValueAsString')")
-
-        return "ok"
+        return jdbcTemplate.query("insert into games (initial) values ('$writeValueAsString') RETURNING id") { rs, _ ->
+            println(rs)
+            rs.getInt(1
+            )
+        }.first()
     }
 
 
